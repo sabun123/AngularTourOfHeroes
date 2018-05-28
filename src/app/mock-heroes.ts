c634bc9c071134078e1ebff822994df01982cb6a
yusuf <yusuf@clarity.my>
2018-05-28 14:52:04 +0800

    List heroes and enable hero selection

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice'},
+    { id: 12, name: 'Bon Jovi'},
+    { id: 13, name: 'Jonah Hex'},
+    { id: 14, name: 'Diana Prince'},
+    { id: 15, name: 'ScarJo'},
+    { id: 16, name: 'Milla Jovovich'},
+    { id: 17, name: 'Krewella'},
+    { id: 18, name: 'OnePunch'}
+];
